cd87e9d525dc393692c500a0dc61fc951ebfc3e2
ztnel <christian@leapsystems.online>
2022-02-06 06:31:09 +0000

    first set of unittests

--- tests/test_pwm.cpp
+++ tests/test_pwm.cpp
@@ -9,7 +9,7 @@ extern "C" {
 
 
 DEFINE_FFF_GLOBALS;
-FAKE_VALUE_FUNC(pwm_code, ioctl, const char*, const void*, size_t);
+FAKE_VALUE_FUNC(pwm_code, ioctl, const char*, const char*, size_t);
 
 class TestPwm : public testing::Test {
   public:
@@ -19,7 +19,60 @@ class TestPwm : public testing::Test {
     }
 };
 
-TEST_F(TestPwm, IoctlCalled) {
-  set_export(true);
+TEST_F(TestPwm, SetExportReturn) {
+  pwm_code mock_ret = PWM_GENERAL_ERROR;
+  ioctl_fake.return_val = mock_ret;
+  int result = set_export(true);
+  ASSERT_EQ(result, mock_ret);
   ASSERT_EQ(ioctl_fake.call_count, 1);
+}
+
+TEST_F(TestPwm, SetExportTrue) {
+  set_export(true);
+  ASSERT_EQ(strcmp(ioctl_fake.arg0_val, EXPORT_PATH), 0);
+}
+
+TEST_F(TestPwm, SetExportFalse) {
+  set_export(false);
+  ASSERT_EQ(strcmp(ioctl_fake.arg0_val, UNEXPORT_PATH), 0);
+}
+
+TEST_F(TestPwm, SetEnableReturn) {
+  pwm_code mock_ret = PWM_GENERAL_ERROR;
+  ioctl_fake.return_val = mock_ret;
+  int result = set_enable(true);
+  ASSERT_EQ(result, mock_ret);
+}
+
+TEST_F(TestPwm, SetEnableTrue) {
+  set_enable(true);
+  ASSERT_EQ(strcmp(ioctl_fake.arg1_val, "1"), 0);
+}
+
+TEST_F(TestPwm, SetEnableFalse) {
+  set_enable(false);
+  ASSERT_EQ(strcmp(ioctl_fake.arg1_val, "0"), 0);
+}
+
+TEST_F(TestPwm, SetDutyReturn) {
+  pwm_code mock_ret = PWM_GENERAL_ERROR;
+  ioctl_fake.return_val = mock_ret;
+  int result = set_duty(1000000);
+  ASSERT_EQ(result, mock_ret);
+}
+
+TEST_F(TestPwm, SetDutyIoctlArgs) {
+  set_duty(200000);
+  ASSERT_EQ(strcmp(ioctl_fake.arg0_val, DUTY_PATH), 0);
+}
+
+TEST_F(TestPwm, SetPeriodReturn) {
+  pwm_code mock_ret = PWM_GENERAL_ERROR;
+  ioctl_fake.return_val = mock_ret;
+  int result = set_period(1000000);
+  ASSERT_EQ(result, mock_ret);
+}
+TEST_F(TestPwm, SetPeriodIoctlArgs) {
+  set_period(200000);
+  ASSERT_EQ(strcmp(ioctl_fake.arg0_val, PERIOD_PATH), 0);
 }
