59ce29c6a7f0bedc8d57abd8c8bfb1f6e7bd2efa
ztnel <christian@leapsystems.online>
2022-02-13 21:40:39 +0000

    add getopt parser for example

--- examples/pwm/main.c
+++ examples/pwm/main.c
@@ -1,7 +1,23 @@
-#include <static/pwm.h>
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief Example usage of PWM driver
+ * @version 0.1
+ * @date 2022-02
+ * 
+ * @copyright Copyright © 2022 Christian Sargusingh
+ * 
+ */
+#include <pwm.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
 
+#define OPTSTR ":hd:p:"
+
+int shutdown();
+void usage();
 
 int shutdown() {
   set_enable(false);
@@ -9,17 +25,55 @@ int shutdown() {
   return 0;
 }
 
-int main(int argc, char **argv) {
-  int status;
-  if (argc != 3) {
-    // error("Usage: %s duty period", argv[0]);
-    return 1;
+void usage() {
+  printf(
+    "Example PWM Driver.\n"
+    "Usage:\n"
+    "  ex-pwm\n"
+    "  ex-pwm -d <ns> -p <ns>\n"
+    "  ex-pwm -h | --help\n\n"
+    "Options:\n"
+    "  -h --help    Show this screen.\n"
+    "  -d <ns>      Duty cycle in nanoseconds.\n"
+    "  -p <ns>      Period in nanoseconds.\n"
+  );
+}
+
+int main(int argc, char *argv[]) {
+  int c, status, errflg;
+  uint64_t duty;
+  uint64_t period;
+  while ((c = getopt(argc, argv, OPTSTR)) != -1) {
+    switch (c) {
+      case 'h':
+        usage();
+        exit(0);
+      case 'd':
+        duty = atoi(optarg);
+        break;
+      case 'p' :
+        period = atoi(optarg);
+        break;
+      case ':':
+        fprintf(stderr, "-%c without argument\n", optopt);
+        errflg++;
+        break;
+      case '?':
+        fprintf(stderr, "unknown argument -%c\n", optopt);
+        errflg++;
+        break;
+      default:
+        abort();
+    }
   }
-  printf("Example PWM Driver\n");
-  printf("==================\n");
-  // read count from argc
-  uint64_t duty = atoi(argv[1]);
-  uint64_t period = atoi(argv[2]);
+  if (errflg) {
+    usage();
+    exit(2);
+  }
+  if (!duty || !period) {
+    usage();
+    exit(0);
+  }
   // set export
   status = set_export(true);
   if (status != 0) {
