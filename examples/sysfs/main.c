3de91605e122f51dff2bf6a7745188b76578daba
cSDes1gn <christian911@sympatico.ca>
2022-02-15 21:57:14 -0500

    add sysfs driver example

---
+++ examples/sysfs/main.c
@@ -0,0 +1,70 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief Example usage of sysfs driver
+ * @version 0.1
+ * @date 2022-02
+ * 
+ * @copyright Copyright © 2022 Christian Sargusingh
+ * 
+ */
+#include <sysfs.h>
+#include <merase.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define OPTSTR ":hc:"
+
+int shutdown();
+void usage();
+
+void usage() {
+  printf(
+    "Example Sysfs Driver.\n"
+    "Usage:\n"
+    "  ex-sysfs\n"
+    "  ex-pwm -c <cmd>\n"
+    "  ex-pwm -h\n\n"
+    "Options:\n"
+    "  -h --help      Show this screen.\n"
+    "  -c <cmd>       Execute linux command\n"
+  );
+}
+
+int main(int argc, char *argv[]) {
+  int c, status, errflg = 0;
+  char *cmd = NULL;
+  logger_set_level(TRACE);
+  while ((c = getopt(argc, argv, OPTSTR)) != -1) {
+    switch (c) {
+      case 'h':
+        usage();
+        exit(0);
+      case 'c':
+        cmd = optarg;
+        break;
+      case ':':
+        fprintf(stderr, "-%c without argument\n", optopt);
+        errflg++;
+        break;
+      case '?':
+        fprintf(stderr, "unknown argument -%c\n", optopt);
+        errflg++;
+        break;
+      default:
+        abort();
+    }
+  }
+  if (errflg) {
+    usage();
+    exit(2);
+  }
+  if (cmd == NULL) {
+    usage();
+    exit(0);
+  }
+  printf("%s", exec_linux_cmd(cmd));
+  return 0;
+}
