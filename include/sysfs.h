7645e2136a1f8eab8fff306ec053a46660b98cd7
ztnel <christian@leapsystems.online>
2022-02-18 06:44:27 +0000

    add polarity path

--- include/sysfs.h
+++ include/sysfs.h
@@ -22,6 +22,7 @@ static const char UNEXPORT_PATH[] = "/sys/class/pwm/pwmchip0/unexport";
 static const char PERIOD_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/period";
 static const char DUTY_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/duty_cycle";
 static const char ENABLE_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/enable";
+static const char POLARITY[] = "/sys/class/pwm/pwmchip0/pwm0/polarity";
 
 
 char *rctl(const char *path, size_t buf_size);
