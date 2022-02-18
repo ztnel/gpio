7537c07ac7c42767be347306681eeb0b82ae547b
ztnel <christian@leapsystems.online>
2022-02-18 08:08:34 +0000

    add initialization functions and structs

--- src/pwm.c
+++ src/pwm.c
@@ -14,48 +14,82 @@
 #include <stdbool.h>
 #include <string.h>
 #include <merase.h>
+#include <pthread.h>
 #include "errors.h"
 #include "pwm.h"
 #include "sysfs.h"
 
-static pwm_iface pwm = {
+
+// constant path definitions
+static const char _EXPORT_PATH[] = "/sys/class/pwm/pwmchip0/export";
+static const char _UNEXPORT_PATH[] = "/sys/class/pwm/pwmchip0/unexport";
+static const char _PERIOD_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/period";
+static const char _DUTY_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/duty_cycle";
+static const char _ENABLE_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/enable";
+static const char _POLARITY_PATH[] = "/sys/class/pwm/pwmchip0/pwm0/polarity";
+
+static struct pwm_iface pwm = {
+  .channel=PWM0,
+  .duty=0,
+  .period=0,
+  .polarity=NORMAL,
+  .enable=false
 };
 
+static void _initialize_state();
+
+/**
+ * @brief Initialize the pwm interface and populate parameters
+ * 
+ */
 void pwm_init() {
   pwm_set_export(true);
-  pwm_read_state();
+  _initialize_state();
+  info("pwm state initialized");
 }
 
-pwm_code pwm_read_state() {
-  char *buf = rctl(DUTY_PATH, 8);
-  pwm->duty = atoi(buf);
-  free_buffer(buf);
-  char *buf = rctl(PERIOD_PATH, 8);
-  pwm->period = atoi(buf);
-  free_buffer(buf);
-  char *buf = rctl(, 8);
-  if (strcmp(buf, "normal") == 0)
-    pwm->polarity = NORMAL;
-  else
-    pwm->polarity = INVERSE;
-  free_buffer(buf);
+/**
+ * @brief Read pwm state variables and populate local struct
+ * 
+ */
+static void _initialize_state() {
+  char *buf;
 
+  buf = rctl(_DUTY_PATH, 8);
+  if (buf != NULL)
+    pwm.duty = atoll(buf);
+    free_buffer(buf);
+
+  buf = rctl(_PERIOD_PATH, 8);
+  if (buf != NULL)
+    pwm.period = atoll(buf);
+    free_buffer(buf);
+
+  buf = rctl(_ENABLE_PATH, 5);
+  if (buf != NULL)
+    pwm.enable = (strcmp(buf, "1") == 0) ? true : false;
+    free_buffer(buf);
+
+  buf = rctl(_POLARITY_PATH, 7);
+  if (buf != NULL)
+    pwm.polarity = (strcmp(buf, "normal") == 0) ? NORMAL : INVERSE;
+    free_buffer(buf);
 }
 
 /**
  * @brief Reserve or close access to pwmchip0
  * 
  * @param reserve reservation flag 
- * @return pwm_code 
+ * @return pwm_status 
  */
-pwm_code pwm_set_export(bool reserve) {
-  pwm_code status;
+pwm_status pwm_set_export(bool reserve) {
+  pwm_status status;
   size_t size = sizeof(uint8_t);
   if (reserve) {
-    status = wctl(EXPORT_PATH, "1", size);
+    status = wctl(_EXPORT_PATH, "1", size);
     trace("pwm export returned status: %d", status);
   } else {
-    status = wctl(UNEXPORT_PATH, "1", size);
+    status = wctl(_UNEXPORT_PATH, "1", size);
     trace("pwm unexport returned status: %d", status);
   }
   return status;
@@ -65,16 +99,16 @@ pwm_code pwm_set_export(bool reserve) {
  * @brief Enable/Disable pwm channel
  * 
  * @param enable flag for enable/disable
- * @return pwm_code 
+ * @return pwm_status 
  */
-pwm_code pwm_set_enable(bool enable) {
-  pwm_code status;
+pwm_status pwm_set_enable(bool enable) {
+  pwm_status status;
   size_t size = sizeof(uint8_t);
   if (enable) {
-    status = wctl(ENABLE_PATH, "1", size);
+    status = wctl(_ENABLE_PATH, "1", size);
     trace("Set pwm enable returned status: %d", status);
   } else {
-    status = wctl(ENABLE_PATH, "0", size);
+    status = wctl(_ENABLE_PATH, "0", size);
     trace("Set pwm disable returned status: %d", status);
   }
   return status;
@@ -84,13 +118,13 @@ pwm_code pwm_set_enable(bool enable) {
  * @brief Set the duty cycle of the pwm waveform in ns
  * 
  * @param duty positive pulse width in ns
- * @return pwm_code 
+ * @return pwm_status 
  */
-pwm_code pwm_set_duty(uint64_t duty) {
+pwm_status pwm_set_duty(uint64_t duty) {
   size_t size;
   char *buf = int64_to_str(duty, &size);
   info("Buffer: %s Size: %d\n", buf, size);
-  pwm_code status = wctl(DUTY_PATH, buf, size);
+  pwm_status status = wctl(_DUTY_PATH, buf, size);
   free_buffer(buf);
   trace("Set pwm duty returned status: %d", status);
   return status;
@@ -100,13 +134,13 @@ pwm_code pwm_set_duty(uint64_t duty) {
  * @brief Set the waveform pulse period in ns 
  * 
  * @param period pulse period in nanoseconds
- * @return pwm_code 
+ * @return pwm_status 
  */
-pwm_code pwm_set_period(uint64_t period) {
+pwm_status pwm_set_period(uint64_t period) {
   size_t size;
   char *buf = int64_to_str(period, &size);
   info("Buffer: %s Size: %d\n", buf, size);
-  pwm_code status = wctl(PERIOD_PATH, buf, size);
+  pwm_status status = wctl(_PERIOD_PATH, buf, size);
   free_buffer(buf);
   trace("Set pwm period returned status: %d", status);
   return status;
