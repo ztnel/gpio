8e0a60c319c020d67a6428e323ef75646311c94c
ztnel <christian@leapsystems.online>
2022-02-19 06:19:26 +0000

    add pthread mock

--- tests/test_sysfs.cpp
+++ tests/test_sysfs.cpp
@@ -25,8 +25,8 @@ DEFINE_FFF_GLOBALS;
 // FAKE_VALUE_FUNC3(ssize_t, write, int, const void *, size_t);
 // FAKE_VALUE_FUNC_VARARG(int, open, const char *, int, ...);
 FAKE_VOID_FUNC_VARARG(merase_log, enum Level, const char *, int, const char *, ...);
-// FAKE_VALUE_FUNC1(int, pthread_mutex_lock, pthread_mutex_t *);
-// FAKE_VALUE_FUNC1(int, pthread_mutex_unlock, pthread_mutex_t *);
+FAKE_VALUE_FUNC1(int, pthread_mutex_lock, pthread_mutex_t *);
+FAKE_VALUE_FUNC1(int, pthread_mutex_unlock, pthread_mutex_t *);
 
 
 class TestSysfs : public testing::Test {
@@ -35,8 +35,8 @@ class TestSysfs : public testing::Test {
       // RESET_FAKE(write);
       // RESET_FAKE(close);
       // RESET_FAKE(open);
-      // RESET_FAKE(pthread_mutex_lock);
-      // RESET_FAKE(pthread_mutex_unlock);
+      RESET_FAKE(pthread_mutex_lock);
+      RESET_FAKE(pthread_mutex_unlock);
       FFF_RESET_HISTORY();
     }
 };
@@ -76,10 +76,10 @@ TEST_F(TestSysfs, wctl_close_failure) {
 //   ASSERT_EQ(pthread_mutex_unlock_fake.call_count, 1);
 }
 
-TEST_F(TestSysfs, Int64toStr) {
+TEST_F(TestSysfs, int_64_to_str) {
   size_t size;
   uint64_t value = 64;
   char *buf = int64_to_str(value, &size);
 //   ASSERT_EQ(strcmp(buf, "64"), 0);
-//   free_buffer(buf);
+  free_buffer(buf);
 }
